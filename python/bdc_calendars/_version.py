00698b85806215d37d1f051675b9ad2d750532a6
Toren Wallengren <toren.wallengren@gmail.com>
2026-09-15 00:14:33 +0100

    Refresh v12 candidate after review fixes

--- python/bdc_calendars/_version.py
+++ python/bdc_calendars/_version.py
@@ -7,7 +7,7 @@ __version__ = "0.12.0"
 DATA_VERSION = "12.0.0"
 
 #: calendar-project commit the data was generated from.
-DATA_GIT_SHA = "adf8dd99893301852aa3daccfb7f0caee22859f3"
+DATA_GIT_SHA = "94db6de7728c26ee75c0449220a721bfdb1977d7"
 
 #: Date the data was generated.
 DATA_GENERATION_DATE = "2026-09-14"
